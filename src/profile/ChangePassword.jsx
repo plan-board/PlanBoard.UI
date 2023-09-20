66bc8ef3ff76fe5abfba346e9803afef12b2502b
mukesh kumar <mukesh.durapid@gmail.com>
2023-09-20 21:16:31 +0530

    forgot and reset password screens done

--- src/profile/ChangePassword.jsx
+++ src/profile/ChangePassword.jsx
@@ -6,40 +6,74 @@ import { useDispatch } from "react-redux";
 const ChangePassword = () => {
     const dispatch = useDispatch();
 
-    const [email, setEmail] = useState("");
-    const [error, setError] = useState(false);
-    const [msg, setMsg] = useState('');  
-    const [password, setPassword] = useState('');  
-    // const [cPassword, setCPassword] = useState('');  
+    const initialFormData = {
+        currentPassword: '',
+        newPassword: '',
+        confirmPassword: '',
+    };
 
-    const submit = async (e) => {
+
+    const [msg, setMsg] = useState('');
+    const [error, setError] = useState('');
+    const [formData, setFormData] = useState(initialFormData);
+
+    // Handle form input changes
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (e) => {
         console.log("-ss");
         e.preventDefault();
+
         try {
-            const payload = { username: email }
-            const response = await axiosInstance.post(
-                "api/Summary/forgot-pass",
-                payload
+            if (formData.newPassword !== formData.confirmPassword) {
+                setError("New Password and Confirm Password do not match.");
+                return;
+            }
+
+            const getUser = await axiosInstance.post(
+                "GetEmployeeData",
+                { Token: localStorage.getItem("access_token") }
             );
-            console.log("=====forgot-pass====", response);
-            if (response?.status === 200) {
-                setMsg("Kindly check your email");
+            console.log("=====getUser====", getUser);
+            if (getUser?.status !== 200) {
+                setError('Something went wrong!')
             } else {
-                setError(true)
+                const payload = {
+                    Token: localStorage.getItem("access_token"),
+                    employee_code: getUser?.data.employee_code,
+                    employee_email: getUser?.data.employee_email,
+                    password: formData.confirmPassword,
+                };
+                const response = await axiosInstance.post(
+                    "SetEmployeePassword",
+                    payload
+                );
+                console.log("=====Update pass====", response);
+                if (response?.status === 200 && response?.data?.Status === true) {
+                    setMsg("Password changed successfully.");
+                    setError('');
+                    resetForm();
+                } else {
+                    setError(response?.data?.Message)
+                }
+
             }
         } catch (error) {
             // Handle errors
-            setError(true)
+            setError('Something went wrong')
             dispatch({ type: SHOW_TOAST, payload: error.message });
         }
     }
-    const setConfirm = (value) =>  {
-        console.log("value", value);
-         
-        // if(password != value){
+    const resetForm = () => {
+        setFormData(initialFormData);
+    };
 
-        // }
-    }
 
     return (
         <div className="main">
@@ -50,23 +84,49 @@ const ChangePassword = () => {
                 <div className="login-box">
                     <h4>Change Password</h4>
                     <div className="tbl-container">
-                        <form onSubmit={submit} >
+                        <form onSubmit={handleSubmit}>
                             <div className="form-group h6">
-                                <input className="w3-input w3-border" type="password" required={true} placeholder="Current Password" onChange={(e) => setEmail(e.target.value)} />
+                                <input
+                                    className="w3-input w3-border"
+                                    type="password"
+                                    name="currentPassword"
+                                    required={true}
+                                    placeholder="Current Password"
+                                    value={formData.currentPassword}
+                                    onChange={handleInputChange}
+                                />
                             </div>
                             <div className="form-group h6">
-                                <input className="w3-input w3-border" type="password" required={true} placeholder="New Password" onChange={(e) => setEmail(e.target.value)} />
+                                <input
+                                    className="w3-input w3-border"
+                                    type="password"
+                                    name="newPassword"
+                                    required={true}
+                                    placeholder="New Password"
+                                    value={formData.newPassword}
+                                    onChange={handleInputChange}
+                                />
                             </div>
                             <div className="form-group h6">
-                                <input className="w3-input w3-border" type="password" required={true} placeholder="Confirm Password" onChange={(e) => setConfirm(e.target.value)} />
+                                <input
+                                    className="w3-input w3-border"
+                                    type="password"
+                                    name="confirmPassword"
+                                    required={true}
+                                    placeholder="Confirm Password"
+                                    value={formData.confirmPassword}
+                                    onChange={handleInputChange}
+                                />
                             </div>
-                            <div className="form-group w3-small h6 ">
-                            </div>
+                            <div className="form-group w3-small h6 "></div>
                             <div className="form-group m-0">
-                                <button className="" type="submit"> Submit </button>
+                                <button type="submit">Submit</button>
                             </div>
                             <div className="form-group w3-text-red">
                                 <p>{error}</p>
+                            </div>
+                            <div className="form-group w3-text-green">
+                                <p>{msg}</p>
                             </div>
                         </form>
                     </div>
